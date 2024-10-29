e47e32534c49f5276fcd2eecb95f1e7e10b5c6c4
Vinicius Felipi <viniciusfelipipereira@gmail.com>
2024-10-29 20:15:13 -0300

    Atualizações e teste de novos metodos

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,34 +5,38 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue, // Definindo a cor azul para a AppBar
-        title: const Center(
-          child: Text(
-            'Titulo',
-            style: TextStyle(
-                fontSize: 20,
-                color: Colors.white), // Tamanho da fonte do título
+          // backgroundColor: Colors.blue, // Definindo a cor azul para a AppBar
+          // title: const Center(
+          //   child: Text(
+          //     'Titulo',
+          //     style: TextStyle(
+          //         fontSize: 20,
+          //         color: Colors.white), // Tamanho da fonte do título
+          //   ),
+          // ),
+          // // Usando um tamanho preferido para evitar problemas de layout
+          // toolbarHeight: 50, // Altura da AppBar
+          // actions: const [
+          //   Center(
+          //     child: Text('X'),
+          //   ),
+          // ],
           ),
-        ),
-        // Usando um tamanho preferido para evitar problemas de layout
-        toolbarHeight: 50, // Altura da AppBar
-        actions: const [
-          Center(
-            child: Text('X'),
-          ),
-        ],
-      ),
       body: Container(
-        width: 200,
-        height: 200,
-        color: const Color.fromARGB(255, 90, 115, 228),
-        child: const Column(
+        width: double.infinity,
+        // height: 200,
+        // color: const Color.fromARGB(255, 90, 115, 228),
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
           children: [
             Text('Conteúdo da página'),
             Text('texto2'),
             Text('texto3'),
+            Container(
+                width: double.infinity,
+                child: ElevatedButton(onPressed: () {}, child: Text('data')))
           ],
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,12 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.yellow,
+        ),
+      ),
       home: HomePage(),
     ),
   );
